0d60a99849865ac263ee2edd108a7e002844a258
Daniel Damiani <ddamiani@slac.stanford.edu>
2020-03-05 10:58:58 -0800

    create config cache for Jungfrau

---
+++ jungfrau/ConfigCache.cc
@@ -0,0 +1,126 @@
+#include "pds/jungfrau/ConfigCache.hh"
+#include "pds/jungfrau/Driver.hh"
+#include "pds/jungfrau/Manager.hh"
+#include "pds/jungfrau/DetectorId.hh"
+
+#include "pds/config/JungfrauConfigType.hh"
+#include "pds/utility/Appliance.hh"
+
+using namespace Pds::Jungfrau;
+
+enum {StaticALlocationNumberOfConfigurationsForScanning=100};
+
+ConfigCache::ConfigCache(const Src& src, Detector& detector, DetIdLookup& lookup, Manager& mgr) :
+  CfgCache(src, _jungfrauConfigType, StaticALlocationNumberOfConfigurationsForScanning * sizeof(JungfrauConfigType)),
+  _occPool(sizeof(UserMessage),1),
+  _detector(detector),
+  _lookup(lookup),
+  _mgr(mgr)
+{}
+
+ConfigCache::~ConfigCache()
+{}
+
+bool ConfigCache::configure()
+{
+  bool error = false;
+  unsigned nrows = 0;
+  unsigned ncols = 0;
+  bool mod_size_set = false;
+  JungfrauConfigType& config = *reinterpret_cast<JungfrauConfigType*>(_cur_config);
+  JungfrauModConfigType module_config[JungfrauConfigType::MaxModulesPerDetector];
+
+  for (unsigned i=0; i<_detector.get_num_modules(); i++) {
+    if (!mod_size_set) {
+      nrows = _detector.get_num_rows(i);
+      ncols = _detector.get_num_columns(i);
+      mod_size_set = true;
+    } else if ((_detector.get_num_rows(i) != nrows) || (_detector.get_num_columns(i) != ncols)) {
+      printf("ConfigCache: detector modules with different shapes are not supported! (%u x %u) vs (%u x %u)\n",
+             nrows, ncols, _detector.get_num_rows(i), _detector.get_num_columns(i));
+      UserMessage* msg = new (&_occPool) UserMessage("Jungfrau Config Error: modules with different shapes are not supported!\n");
+      _mgr.appliance().post(msg);
+      error = true;
+    }
+  }
+
+  if (!mod_size_set) {
+    printf("ConfigCache: detector seems to have no modules to configure!\n");
+    UserMessage* msg = new (&_occPool) UserMessage("Jungfrau Config Error: detector seems to have no modules to configure!\n");
+    _mgr.appliance().post(msg);
+    error = true;
+  }
+
+  if (!error) {
+    // Retrieve the module specific read-only configuration info to update config object
+    if (!_detector.get_module_config(module_config)) {
+      printf("ConfigCache: failed to retrieve module version information!\n");
+      UserMessage* msg = new (&_occPool) UserMessage("Jungfrau Config Error: failed to retrieve module version information!\n");
+      _mgr.appliance().post(msg);
+      error = true;
+    }
+
+    for (unsigned i=0; i<_detector.get_num_modules(); i++) {
+      const char* hostname = _detector.get_hostname(i);
+      if (hostname) {
+        if(_lookup.has(hostname))
+          JungfrauModConfig::setSerialNumber(module_config[i], _lookup[hostname].full());
+      } else {
+        printf("ConfigCache: module %d appears to have no hostname to use for serial number lookup!\n", i);
+        UserMessage* msg = new (&_occPool) UserMessage("Jungfrau Config Error: failed to lookup serial number!\n");
+        _mgr.appliance().post(msg);
+        error = true;
+      }
+      printf("Module %u version info:\n"
+             "  serial number:  0x%lx\n"
+             "  version number: 0x%lx\n"
+             "  firmware:       0x%lx\n",
+             i,
+             module_config[i].serialNumber(),
+             module_config[i].moduleVersion(),
+             module_config[i].firmwareVersion());
+    }
+
+    JungfrauConfig::setSize(config, _detector.get_num_modules(), nrows, ncols, module_config);
+    DacsConfig dacs_config(config.vb_ds(), config.vb_comp(), config.vb_pixbuf(), config.vref_ds(),
+                           config.vref_comp(), config.vref_prech(), config.vin_com(), config.vdd_prot());
+
+    if (!_detector.configure(0, config.gainMode(), config.speedMode(),
+                             config.triggerDelay(), config.exposureTime(),
+                             config.exposurePeriod(), config.biasVoltage(),
+                             dacs_config)) {
+      error = true;
+    }
+
+    if (!_detector.check_size(config.numberOfModules(),
+                              config.numberOfRowsPerModule(),
+                              config.numberOfColumnsPerModule())) {
+      error = true;
+    }
+
+    if (error) {
+      printf("ConfigCache: failed to apply configuration.\n");
+
+      const char** errors = _detector.errors();
+      for (unsigned i=0; i<_detector.get_num_modules(); i++) {
+        if (strlen(errors[i])) {
+          UserMessage* msg = new (&_occPool) UserMessage("Jungfrau Config ");
+          msg->append(errors[i]);
+          _mgr.appliance().post(msg);
+        }
+      }
+      _detector.clear_errors();
+    }
+  }
+
+  if (error) {
+    _configtc.damage.increase(Damage::UserDefined);
+  }
+
+  return !error;
+}
+
+int ConfigCache::_size(void* tc) const
+{
+  return sizeof(JungfrauConfigType);
+}
---
+++ jungfrau/ConfigCache.hh
@@ -0,0 +1,33 @@
+#ifndef Pds_Jungfrau_ConfigCache_hh
+#define Pds_Jungfrau_ConfigCache_hh
+
+#include "pds/config/CfgCache.hh"
+#include "pds/service/GenericPool.hh"
+#include <vector>
+
+namespace Pds {
+  class Datagram;
+  class Task;
+  namespace Jungfrau {
+    class Detector;
+    class DetIdLookup;
+    class Manager;
+    class ConfigCache : public CfgCache {
+    public:
+      ConfigCache(const Src& src, Detector& detector, DetIdLookup& lookup, Manager& mgr);
+      virtual ~ConfigCache();
+    public:
+      bool configure();
+    private:
+      int   _size (void*) const;
+    private:
+      GenericPool    _occPool;
+      Detector&      _detector;
+      DetIdLookup&   _lookup;
+      Manager&       _mgr;
+    };
+  };
+};
+
+#endif
+

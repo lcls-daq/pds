9f405c788d6263567efac03853db4edadecddba6
Daniel Damiani <ddamiani@slac.stanford.edu>
2021-04-01 11:17:41 -0700

    add ControlData::ConfigV4

--- config/ControlConfigType.cc
+++ config/ControlConfigType.cc
@@ -1,16 +1,12 @@
 #include "pds/config/ControlConfigType.hh"
 
-using Pds::ControlData::PVControl;
-using Pds::ControlData::PVMonitor;
-using Pds::ControlData::PVLabel;
+static bool compare_control(const PVControlType& a, const PVControlType& b) { return strcmp(a.name(),b.name())<=0; }
+static bool compare_monitor(const PVMonitorType& a, const PVMonitorType& b) { return strcmp(a.name(),b.name())<=0; }
+static bool compare_label  (const PVLabelType  & a, const PVLabelType  & b) { return strcmp(a.name(),b.name())<=0; }
 
-static bool compare_control(const PVControl& a, const PVControl& b) { return strcmp(a.name(),b.name())<=0; }
-static bool compare_monitor(const PVMonitor& a, const PVMonitor& b) { return strcmp(a.name(),b.name())<=0; }
-static bool compare_label  (const PVLabel  & a, const PVLabel  & b) { return strcmp(a.name(),b.name())<=0; }
-
-static PVControl* list_to_array(const std::list<PVControl>&);
-static PVMonitor* list_to_array(const std::list<PVMonitor>&);
-static PVLabel  * list_to_array(const std::list<PVLabel  >&);
+static PVControlType* list_to_array(const std::list<PVControlType>&);
+static PVMonitorType* list_to_array(const std::list<PVMonitorType>&);
+static PVLabelType  * list_to_array(const std::list<PVLabelType  >&);
 
 ControlConfigType* Pds::ControlConfig::_new(void* p)
 {
@@ -18,14 +14,14 @@ ControlConfigType* Pds::ControlConfig::_new(void* p)
 }
 
 ControlConfigType* Pds::ControlConfig::_new(void* p, 
-                                            const std::list<PVControl>& controls,
-                                            const std::list<PVMonitor>& monitors,
-                                            const std::list<PVLabel>&   labels,
+                                            const std::list<PVControlType>& controls,
+                                            const std::list<PVMonitorType>& monitors,
+                                            const std::list<PVLabelType>&   labels,
                                             const Pds::ClockTime& ctime)
 {
-  PVControl* c = list_to_array(controls);
-  PVMonitor* m = list_to_array(monitors);
-  PVLabel  * l = list_to_array(labels);
+  PVControlType* c = list_to_array(controls);
+  PVMonitorType* m = list_to_array(monitors);
+  PVLabelType  * l = list_to_array(labels);
   ControlConfigType* r = new(p) ControlConfigType(0, 0, 1, 0, ctime,
                                                   controls.size(),
                                                   monitors.size(),
@@ -40,14 +36,14 @@ ControlConfigType* Pds::ControlConfig::_new(void* p,
 }
 
 ControlConfigType* Pds::ControlConfig::_new(void* p, 
-                                            const std::list<PVControl>& controls,
-                                            const std::list<PVMonitor>& monitors,
-                                            const std::list<PVLabel>&   labels,
+                                            const std::list<PVControlType>& controls,
+                                            const std::list<PVMonitorType>& monitors,
+                                            const std::list<PVLabelType>&   labels,
                                             unsigned events)
 {
-  PVControl* c = list_to_array(controls);
-  PVMonitor* m = list_to_array(monitors);
-  PVLabel  * l = list_to_array(labels);
+  PVControlType* c = list_to_array(controls);
+  PVMonitorType* m = list_to_array(monitors);
+  PVLabelType  * l = list_to_array(labels);
   ControlConfigType* r = new(p) ControlConfigType(events, 0, 0, 1, ClockTime(0,0),
                                                   controls.size(),
                                                   monitors.size(),
@@ -62,14 +58,14 @@ ControlConfigType* Pds::ControlConfig::_new(void* p,
 }
 
 ControlConfigType* Pds::ControlConfig::_new(void* p, 
-                                            const std::list<PVControl>& controls,
-                                            const std::list<PVMonitor>& monitors,
-                                            const std::list<PVLabel>&   labels,
+                                            const std::list<PVControlType>& controls,
+                                            const std::list<PVMonitorType>& monitors,
+                                            const std::list<PVLabelType>&   labels,
                                             L3TEvents events)
 {
-  PVControl* c = list_to_array(controls);
-  PVMonitor* m = list_to_array(monitors);
-  PVLabel  * l = list_to_array(labels);
+  PVControlType* c = list_to_array(controls);
+  PVMonitorType* m = list_to_array(monitors);
+  PVLabelType  * l = list_to_array(labels);
   ControlConfigType* r = new(p) ControlConfigType(events, 1, 0, 0, ClockTime(0,0),
                                                   controls.size(),
                                                   monitors.size(),
@@ -83,42 +79,152 @@ ControlConfigType* Pds::ControlConfig::_new(void* p,
   return r;
 }
 
-PVControl* list_to_array(const std::list<PVControl>& pvcs)
+size_t Pds::ControlConfig::convert(void* p,
+                                   const ControlConfigType& orig,
+                                   uint32_t version)
 {
-  PVControl* c = 0;
+  using Pds::ControlData::PVControl;
+  using Pds::ControlData::PVMonitor;
+  using Pds::ControlData::PVLabel;
+
+  switch (version) {
+  case 0:
+    { Pds::ControlData::ConfigV3* cfg = 0;
+      PVControl* c = 0;
+      PVMonitor* m = 0;
+      PVLabel  * l = 0;
+
+      if (orig.npvControls()!=0) {
+        c = new PVControl[orig.npvControls()];
+        ndarray<const ControlData::PVControlV1, 1> pvcs = orig.pvControls();
+        for (unsigned i=0; i<orig.npvControls(); i++) {
+          c[i] = PVControl(pvcs[i].name(),
+                           pvcs[i].index(),
+                           pvcs[i].value());
+        }
+      }
+
+      if (orig.npvMonitors()!=0) {
+        m = new PVMonitor[orig.npvMonitors()];
+        ndarray<const ControlData::PVMonitorV1, 1> pvms = orig.pvMonitors();
+        for (unsigned i=0; i<orig.npvMonitors(); i++) {
+          m[i] = PVMonitor(pvms[i].name(),
+                           pvms[i].index(),
+                           pvms[i].loValue(),
+                           pvms[i].hiValue());
+        }
+      }
+
+      if (orig.npvLabels()!=0) {
+        l = new PVLabel[orig.npvLabels()];
+        ndarray<const ControlData::PVLabelV1, 1> pvls = orig.pvLabels();
+        for (unsigned i=0; i<orig.npvLabels(); i++) {
+          l[i] = PVLabel(pvls[i].name(),
+                         pvls[i].value());
+        }
+      }
+
+      cfg = new(p) Pds::ControlData::ConfigV3(orig.events(),
+                                              orig.uses_l3t_events(),
+                                              orig.uses_duration(),
+                                              orig.uses_events(),
+                                              orig.duration(),
+                                              orig.npvControls(),
+                                              orig.npvMonitors(),
+                                              orig.npvLabels(),
+                                              c, m, l);
+      return cfg->_sizeof();
+    }
+    break;
+  default:
+    return 0;
+  }
+}
+
+uint32_t Pds::ControlConfig::pvControlNameSize(uint32_t version)
+{
+  switch (version) {
+  case ControlConfigApiVersion:
+    return PVControlType::NameSize;
+  case 0:
+    return Pds::ControlData::PVControl::NameSize;
+  default:
+    return 0;
+  }
+}
+
+uint32_t Pds::ControlConfig::pvMonitorNameSize(uint32_t version)
+{
+  switch (version) {
+  case ControlConfigApiVersion:
+    return PVMonitorType::NameSize;
+  case 0:
+    return Pds::ControlData::PVMonitor::NameSize;
+  default:
+    return 0;
+  }
+}
+
+uint32_t Pds::ControlConfig::pvLabelNameSize(uint32_t version)
+{
+  switch (version) {
+  case ControlConfigApiVersion:
+    return PVLabelType::NameSize;
+  case 0:
+    return Pds::ControlData::PVLabel::NameSize;
+  default:
+    return 0;
+  }
+}
+
+uint32_t Pds::ControlConfig::pvLabelValueSize(uint32_t version)
+{
+  switch (version) {
+  case ControlConfigApiVersion:
+    return PVLabelType::ValueSize;
+  case 0:
+    return Pds::ControlData::PVLabel::ValueSize;
+  default:
+    return 0;
+  }
+}
+
+PVControlType* list_to_array(const std::list<PVControlType>& pvcs)
+{
+  PVControlType* c = 0;
   if (pvcs.size()!=0) {
-    std::list<PVControl> sorted_pvcs (pvcs); sorted_pvcs.sort(compare_control);
-    c = new PVControl[pvcs.size()];
+    std::list<PVControlType> sorted_pvcs (pvcs); sorted_pvcs.sort(compare_control);
+    c = new PVControlType[pvcs.size()];
     unsigned i=0;
-    for(std::list<PVControl>::const_iterator iter = sorted_pvcs.begin();
+    for(std::list<PVControlType>::const_iterator iter = sorted_pvcs.begin();
         iter != sorted_pvcs.end(); ++iter)
       c[i++] = *iter;
   }
   return c;
 }
 
-PVMonitor* list_to_array(const std::list<PVMonitor>& pvcs)
+PVMonitorType* list_to_array(const std::list<PVMonitorType>& pvcs)
 {
-  PVMonitor* c = 0;
+  PVMonitorType* c = 0;
   if (pvcs.size()!=0) {
-    std::list<PVMonitor> sorted_pvcs (pvcs); sorted_pvcs.sort(compare_monitor);
-    c = new PVMonitor[pvcs.size()];
+    std::list<PVMonitorType> sorted_pvcs (pvcs); sorted_pvcs.sort(compare_monitor);
+    c = new PVMonitorType[pvcs.size()];
     unsigned i=0;
-    for(std::list<PVMonitor>::const_iterator iter = sorted_pvcs.begin();
+    for(std::list<PVMonitorType>::const_iterator iter = sorted_pvcs.begin();
         iter != sorted_pvcs.end(); ++iter)
       c[i++] = *iter;
   }
   return c;
 }
 
-PVLabel  * list_to_array(const std::list<PVLabel  >& pvcs)
+PVLabelType  * list_to_array(const std::list<PVLabelType  >& pvcs)
 {
-  PVLabel  * c = 0;
+  PVLabelType  * c = 0;
   if (pvcs.size()!=0) {
-    std::list<PVLabel  > sorted_pvcs (pvcs); sorted_pvcs.sort(compare_label);
-    c = new PVLabel  [pvcs.size()];
+    std::list<PVLabelType  > sorted_pvcs (pvcs); sorted_pvcs.sort(compare_label);
+    c = new PVLabelType  [pvcs.size()];
     unsigned i=0;
-    for(std::list<PVLabel  >::const_iterator iter = sorted_pvcs.begin();
+    for(std::list<PVLabelType  >::const_iterator iter = sorted_pvcs.begin();
         iter != sorted_pvcs.end(); ++iter)
       c[i++] = *iter;
   }
--- config/ControlConfigType.hh
+++ config/ControlConfigType.hh
@@ -6,13 +6,18 @@
 
 #include <list>
 
-typedef Pds::ControlData::ConfigV3 ControlConfigType;
+typedef Pds::ControlData::ConfigV4 ControlConfigType;
+typedef Pds::ControlData::PVControlV1 PVControlType;
+typedef Pds::ControlData::PVMonitorV1 PVMonitorType;
+typedef Pds::ControlData::PVLabelV1 PVLabelType;
 
 static Pds::TypeId _controlConfigType(Pds::TypeId::Id_ControlConfig,
 				      ControlConfigType::Version);
 
 static const Pds::ClockTime EndCalibTime(0,0);
 static const Pds::ClockTime EndRunTime  (0,1);
+
+static const uint32_t ControlConfigApiVersion = 1;
 
 namespace Pds {
   namespace ControlConfig {
@@ -27,20 +32,29 @@ namespace Pds {
 
     ControlConfigType* _new(void*);
     ControlConfigType* _new(void*, 
-                            const std::list<Pds::ControlData::PVControl>&,
-                            const std::list<Pds::ControlData::PVMonitor>&,
-                            const std::list<Pds::ControlData::PVLabel>&,
+                            const std::list<PVControlType>&,
+                            const std::list<PVMonitorType>&,
+                            const std::list<PVLabelType>&,
                             const Pds::ClockTime&);
     ControlConfigType* _new(void*, 
-                            const std::list<Pds::ControlData::PVControl>&,
-                            const std::list<Pds::ControlData::PVMonitor>&,
-                            const std::list<Pds::ControlData::PVLabel>&,
+                            const std::list<PVControlType>&,
+                            const std::list<PVMonitorType>&,
+                            const std::list<PVLabelType>&,
                             unsigned events);
     ControlConfigType* _new(void*, 
-                            const std::list<Pds::ControlData::PVControl>&,
-                            const std::list<Pds::ControlData::PVMonitor>&,
-                            const std::list<Pds::ControlData::PVLabel>&,
+                            const std::list<PVControlType>&,
+                            const std::list<PVMonitorType>&,
+                            const std::list<PVLabelType>&,
                             L3TEvents events);
+
+    size_t convert(void*,
+                   const ControlConfigType&,
+                   uint32_t version);
+
+    uint32_t pvControlNameSize(uint32_t version);
+    uint32_t pvMonitorNameSize(uint32_t version);
+    uint32_t pvLabelNameSize  (uint32_t version);
+    uint32_t pvLabelValueSize (uint32_t version);
   }
 }
 

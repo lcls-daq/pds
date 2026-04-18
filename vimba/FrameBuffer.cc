e6f5567190d64368f7e6bd8b7d0f8574ff0bcc60
Daniel Damiani <ddamiani@slac.stanford.edu>
2026-04-17 21:18:32 -0700

    make frame buffer more generic for use by the daq and standalone processes

--- vimba/FrameBuffer.cc
+++ vimba/FrameBuffer.cc
@@ -7,13 +7,13 @@
 
 #include <algorithm>
 #include <cstdio>
+#include <cstring>
 
 using namespace Pds::Vimba;
 
-FrameBuffer::FrameBuffer(size_t nbuffers, Camera* cam, Server* srv):
-  _nbuffers(nbuffers),
+FrameBuffer::FrameBuffer(size_t nbuffers, Camera* cam):
   _cam(cam),
-  _srv(srv),
+  _nbuffers(nbuffers),
   _frames(new VmbFrame_t[nbuffers]),
   _buffer(NULL),
   _buffer_sz(0)
@@ -45,8 +45,7 @@ void FrameBuffer::configure()
     for (size_t n=0; n<_nbuffers; n++) {
       _frames[n].buffer = _buffer + (payloadSize * n);
       _frames[n].bufferSize = payloadSize;
-      _frames[n].context[0] = _cam;
-      _frames[n].context[1] = _srv;
+      _frames[n].context[0] = this;
     }
 
     // register allocated frames
@@ -55,13 +54,6 @@ void FrameBuffer::configure()
     _cam->captureStart();
     // queue frames
     _cam->queueFrames(_frames, _nbuffers, frameCallBack);
-
-    // show readout buffer information
-    printf("Buffer information:\n");
-    printf("  Driver Buffer Count:    %lld\n", _cam->maxDriverBuffersCount());
-    printf("  Stream Buffer Mode:     %s\n",   _cam->streamBufferHandlingMode());
-    printf("  Stream Buffer Minimum:  %lld\n", _cam->streamAnnounceBufferMinimum());
-    printf("  Stream Buffer Count:    %lld\n", _cam->streamAnnouncedBufferCount());
   }
 }
 
@@ -171,8 +163,133 @@ bool FrameBuffer::copyAs16BitStd(VmbFrame_t* frame, void* buffer)
 
 void VMB_CALL FrameBuffer::frameCallBack(const VmbHandle_t hcam, const VmbHandle_t hstream, VmbFrame_t* ptr)
 {
-  Server* srv = reinterpret_cast<Server*>(ptr->context[1]);
-  if (srv->post(ptr)) {
-    VmbCaptureFrameQueue(hcam, ptr, frameCallBack);
+  FrameBuffer* framebuf = reinterpret_cast<FrameBuffer*>(ptr->context[0]);
+  // process the frame
+  framebuf->process(ptr);
+  // requeue frame
+  VmbCaptureFrameQueue(hcam, ptr, frameCallBack);
+}
+
+SimpleFrameBuffer::SimpleFrameBuffer(size_t nbuffers, size_t nframes, Camera* cam, const char* file_prefix, bool show_stats, bool reformat_pixels):
+  FrameBuffer(nbuffers, cam),
+  _nframes(nframes),
+  _ncomp(0),
+  _file_prefix(file_prefix),
+  _show_stats(show_stats),
+  _reformat_pixels(reformat_pixels),
+  _sem(Semaphore::FULL),
+  _fname(file_prefix ? new char[std::strlen(file_prefix) + 15] : NULL)
+{}
+
+SimpleFrameBuffer::~SimpleFrameBuffer()
+{
+  if (_fname) {
+    delete[] _fname;
   }
+}
+
+bool SimpleFrameBuffer::enable()
+{
+  _ncomp = 0;
+  bool started = FrameBuffer::enable();
+  if (started) {
+    _sem.take();
+  }
+  return started;
+}
+
+bool SimpleFrameBuffer::disable()
+{
+  bool stopped = FrameBuffer::disable();
+  if (stopped) {
+    _sem.give();
+  }
+  return stopped;
+}
+
+void SimpleFrameBuffer::wait()
+{
+  _sem.take();
+}
+
+void SimpleFrameBuffer::cancel()
+{
+  _sem.give();
+}
+
+void SimpleFrameBuffer::process(VmbFrame_t* frame)
+{
+  // ignore extra frames on fixed acquisition
+  if (_nframes > 0 && _ncomp >= _nframes) return;
+
+  if (frame->receiveStatus == VmbFrameStatusComplete) {
+    if (_nframes == 0) {
+      printf("\033[32mRecieved frame %zu\033[0m\n", _ncomp+1);
+    } else {
+      printf("\033[32mRecieved frame %zu of %zu\033[0m\n", _ncomp+1, _nframes);
+    }
+  } else {
+    if (_nframes == 0) {
+      printf("\033[31mError recieving frame %zu: %s\033[0m\n",
+             _ncomp+1, FrameStatusCodes::desc(frame->receiveStatus));
+    } else {
+      printf("\033[31mError recieving frame %zu of %zu: %s\033[0m\n",
+             _ncomp+1, _nframes, FrameStatusCodes::desc(frame->receiveStatus));
+    }
+  }
+  if (_show_stats) {
+    printf("timestamp: %llu\n", frame->timestamp);
+    printf("status: %s\n", FrameStatusCodes::desc(frame->receiveStatus));
+    printf("framed id %llu\n", frame->frameID);
+    printf("format %s\n", PixelFormatTypes::desc(frame->pixelFormat));
+  }
+  _ncomp++;
+
+  // write images to file if requested
+  if (_file_prefix && _fname) {
+    sprintf(_fname, "%s%04zu.raw", _file_prefix, _ncomp);
+    FILE *img = fopen(_fname, "wb");
+    if (_reformat_pixels) {
+      VmbUint32_t convertedImageSize = FrameBuffer::sizeAs16Bit(frame);
+      char* convertedImage = new char[convertedImageSize];
+      FrameBuffer::copyAs16Bit(frame, convertedImage);
+      fwrite(convertedImage, 1, convertedImageSize, img);
+      delete[] convertedImage;
+    } else {
+      fwrite(frame->buffer, 1, frame->bufferSize, img);
+    }
+    fclose(img);
+  }
+
+  // single that acquistion is done
+  if (_ncomp == _nframes) {
+    _sem.give();
+  }
+}
+
+ServerFrameBuffer::ServerFrameBuffer(size_t nbuffers, Camera* cam, Server* srv) :
+  FrameBuffer(nbuffers, cam),
+  _srv(srv)
+{}
+
+ServerFrameBuffer::~ServerFrameBuffer()
+{}
+
+void ServerFrameBuffer::configure()
+{
+  // call the base class implementation
+  FrameBuffer::configure();
+  if (_cam) {
+    // show readout buffer information
+    printf("Buffer information:\n");
+    printf("  Driver Buffer Count:    %lld\n", _cam->maxDriverBuffersCount());
+    printf("  Stream Buffer Mode:     %s\n",   _cam->streamBufferHandlingMode());
+    printf("  Stream Buffer Minimum:  %lld\n", _cam->streamAnnounceBufferMinimum());
+    printf("  Stream Buffer Count:    %lld\n", _cam->streamAnnouncedBufferCount());
+  }
+}
+
+void ServerFrameBuffer::process(VmbFrame_t* frame)
+{
+  _srv->post(frame);
 }
--- vimba/FrameBuffer.hh
+++ vimba/FrameBuffer.hh
@@ -1,6 +1,8 @@
 #ifndef Pds_Vimba_FrameBuffer_hh
 #define Pds_Vimba_FrameBuffer_hh
 
+#include "pds/service/Semaphore.hh"
+
 #include "VmbC/VmbC.h"
 
 namespace Pds {
@@ -9,14 +11,16 @@ namespace Pds {
     class Server;
     class FrameBuffer {
       public:
-        FrameBuffer(size_t nbuffers, Camera* cam, Server* srv);
+        FrameBuffer(size_t nbuffers, Camera* cam);
         virtual ~FrameBuffer();
 
-        void configure();
-        void unconfigure();
+        virtual void configure();
+        virtual void unconfigure();
 
-        bool enable();
-        bool disable();
+        virtual bool enable();
+        virtual bool disable();
+
+        virtual void process(VmbFrame_t* frame) = 0;
 
         static VmbUint32_t sizeAs16Bit(VmbFrame_t* frame);
         static bool copyAs16Bit(VmbFrame_t* frame, void* buffer);
@@ -24,13 +28,49 @@ namespace Pds {
       protected:
         static bool copyAs16BitVmb(VmbFrame_t* frame, void* buffer);
         static bool copyAs16BitStd(VmbFrame_t* frame, void* buffer);
+
+        Camera*      _cam;
       private:
         size_t       _nbuffers;
-        Camera*      _cam;
-        Server*      _srv;
         VmbFrame_t*  _frames;
         char*        _buffer;
         size_t       _buffer_sz;
+    };
+
+    class SimpleFrameBuffer : public FrameBuffer {
+      public:
+        SimpleFrameBuffer(size_t nbuffers, size_t nframes, Camera* cam, const char* file_prefix, bool show_stats, bool reformat_pixels);
+        virtual ~SimpleFrameBuffer();
+
+        virtual bool enable() override;
+        virtual bool disable() override;
+
+        void wait();
+        void cancel();
+
+        virtual void process(VmbFrame_t* frame) override;
+
+      private:
+        size_t       _nframes;
+        size_t       _ncomp;
+        const char*  _file_prefix;
+        bool         _show_stats;
+        bool         _reformat_pixels;
+        Semaphore    _sem;
+        char*        _fname;
+    };
+
+    class ServerFrameBuffer : public FrameBuffer {
+      public:
+        ServerFrameBuffer(size_t nbuffers, Camera* cam, Server* srv);
+        virtual ~ServerFrameBuffer();
+
+        virtual void configure() override;
+
+        virtual void process(VmbFrame_t* frame) override;
+
+      private:
+        Server*      _srv;
     };
   }
 }
